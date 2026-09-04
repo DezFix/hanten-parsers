641e0b59eb3e4ac4a6d824175e5d2a366da87c53
DezFix <dezfix@github.com>
2026-09-04 20:19:13 +0300

    MangaChan: im.manga-chan.me primary domain
    Site migrates to im. subdomain (root redirects there, all manga links absolute to im.). Details 502 confirmed IP-based: full pages served to other egress.

--- src/main/kotlin/hanten/wre/app/parsers/site/ru/multichan/MangaChanParser.kt
+++ src/main/kotlin/hanten/wre/app/parsers/site/ru/multichan/MangaChanParser.kt
@@ -7,5 +7,7 @@ import hanten.wre.app.parsers.model.MangaParserSource
 
 @MangaSourceParser("MANGACHAN", "Манга-тян", "ru")
 internal class MangaChanParser(context: MangaLoaderContext) : ChanParser(context, MangaParserSource.MANGACHAN) {
-	override val configKeyDomain = ConfigKey.Domain("manga-chan.me")
+	// The site is migrating to the im. subdomain: the root page redirects there
+	// and all manga links point to im.manga-chan.me
+	override val configKeyDomain = ConfigKey.Domain("im.manga-chan.me", "manga-chan.me")
 }
